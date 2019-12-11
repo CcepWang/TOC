2263ece4ed0ca7333cd0f953a05d56c77bab7e29
CcepWang <zxcasdqwe0258@yahoo.com.tw>
2019-12-11 11:18:31 +0800

    Add code

--- fsm.py
+++ fsm.py
@@ -2,6 +2,8 @@ from transitions.extensions import GraphMachine
 
 from utils import send_text_message
 
+import requests
+from bs4 import BeautifulSoup
 
 class TocMachine(GraphMachine):
     def __init__(self, **machine_configs):
@@ -15,11 +17,25 @@ class TocMachine(GraphMachine):
         text = event.message.text
         return text.lower() == "go to state2"
 
+    def is_going_to_state3(self, event):
+        text = event.message.text
+        return text.lower() == "go to state3"
+
     def on_enter_state1(self, event):
         print("I'm entering state1")
 
+        r = requests.get("https://www.ptt.cc/bbs/MobileComm/index.html")
+        r.encoding = r.apparent_encoding
+        soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+        sel = soup.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+
+        a = ""
+        for s in sel:
+            a=a+(s.text)
+            a=a+"\n"
+
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state1")
+        send_text_message(reply_token, a)
         self.go_back()
 
     def on_exit_state1(self):
@@ -28,9 +44,40 @@ class TocMachine(GraphMachine):
     def on_enter_state2(self, event):
         print("I'm entering state2")
 
+        r = requests.get("https://www.ptt.cc/bbs/joke/index.html")
+        r.encoding = r.apparent_encoding
+        soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+        sel = soup.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+
+        b = ""
+        for s in sel:
+            b=b+(s.text)
+            b=b+"\n"
+
         reply_token = event.reply_token
-        send_text_message(reply_token, "Trigger state2")
+        send_text_message(reply_token, b)
         self.go_back()
 
     def on_exit_state2(self):
         print("Leaving state2")
+
+    def on_enter_state3(self, event):
+        print("I'm entering state3")
+
+        r = requests.get("https://www.ptt.cc/bbs/NBA/index.html")
+        r.encoding = r.apparent_encoding
+        soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+        sel = soup.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+
+        c = ""
+        for s in sel:
+            c=c+(s.text)
+            c=c+"\n"
+
+
+        reply_token = event.reply_token
+        send_text_message(reply_token, c)
+        self.go_back()
+
+    def on_exit_state3(self):
+        print("Leaving state3")
--- test.py
+++ test.py
@@ -1,17 +1,46 @@
 import requests
 from bs4 import BeautifulSoup
 
-r = requests.get("https://www.cwb.gov.tw/V7/forecast/taiwan/Tainan_City.htm")
-#r.encoding = "gbk"
+r = requests.get("https://www.ptt.cc/bbs/NBA/index.html")
 r.encoding = r.apparent_encoding
+soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
 
-soup = BeautifulSoup(r.text,"html.parser")
+#u = soup.select("div.btn-group.btn-group-paging a")#上一頁按鈕的a標籤
+#url = "https://www.ptt.cc"+ u[1]["href"] #組合出上一頁的網址
 
-data=soup.find_all("table", class_="FcstBoxTable01")
+#import requests
+#from bs4 import BeautifulSoup
+#url = "https://www.ptt.cc/bbs/joke/index.html"
+#for i in range(3): #往上爬3頁
+ #   r = requests.get(url)
+ #   soup = BeautifulSoup(r.text,"html.parser")
+ #   sel = soup.select("div.title a") #標題
+ #   u = soup.select("div.btn-group.btn-group-paging a") #a標籤
+ #   #print ("本頁的URL為"+url)
+ #   url = "https://www.ptt.cc"+ u[1]["href"] #上一頁的網址
+
+#a = ""
+#for s in sel: #印出網址跟標題
+#    a=a+(s.text)
+#    a=a+"\n"
+#print(a)
+
+sel = soup.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel
+print(sel)
+
+a = ""
+for s in sel:
+    a=a+(s.text)
+    a=a+"\n"
+print(a)
+#print(list_a) 
+#soup = BeautifulSoup(r.text,"html.parser")
+
+#data=soup.find_all("table", class_="FcstBoxTable01")
 
 #data2=soup.find_all("tbody")
 
-print(data)
+#print(data)
 
 
 #print(r.encoding) #查看網頁返回的字符集類型
